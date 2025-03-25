1405a6f0d9b687132a2f52a7599f3f226b747882
yode <styode@gmail.com>
2025-03-25 18:56:13 +0800

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/prosemirroreditor/',
+  base: '/prose-mirror-editor/',
   plugins: [
     vue(),
     vueDevTools(),
